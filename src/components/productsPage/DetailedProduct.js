e4ddd00c3a661e7a6051b29ac78232a481f62eb8
sorrow112 <jguirimahmed111@gmail.com>
2022-02-25 17:06:43 +0100

    works on details page (cleaning required)

--- src/components/productsPage/DetailedProduct.js
+++ src/components/productsPage/DetailedProduct.js
@@ -1,5 +1,5 @@
-import { Grid, Typography, Button } from "@mui/material";
-import { minHeight } from "@mui/system";
+import { Grid, Typography, Button, TextField } from "@mui/material";
+import { Box, minHeight } from "@mui/system";
 import React from "react";
 import PersonIcon from "@mui/icons-material/Person";
 // import img1 from "../../media/images/demosToBeReplaced/pc1.jpg"
@@ -21,7 +21,8 @@ const DetailedProduct = () => {
   const enchereData = {
     quantity: "quantity",
     initPrice: "initPrice",
-    immediatePrice: "immediatePrice",
+    immediatePrice: 2500,
+    prixActuel: 1000,
     startDate: "startDate",
     endDate: "endDate",
     category: "category",
@@ -44,56 +45,173 @@ const DetailedProduct = () => {
       path: "../../media/images/demosToBeReplaced/pc4.jpg",
     },
   };
+  //#region states
+  const [augmentation, setAugmentation] = React.useState(0.1);
+  const [prixActuel, setPrixActuel] = React.useState(1000);
+  //#endregion
+
+  //#region state handlers
+  const handleAugmentation = (event) => {
+    setAugmentation(parseFloat(event.target.value));
+  };
+  //#endregion
 
   return (
-    <Grid container>
+    <Grid container sx={{ mt: 10 }}>
       {/* could be used for anything */}
       <Grid item xs={2}></Grid>
-      {/* first section */}
-      <Grid container xs={10}>
-        <Grid item xs={5}>
-          <Grid item>
-            <Typography variant="h3">{articleData.name}</Typography>
-          </Grid>
+
+      <Grid
+        container
+        xs={10}
+        sx={{
+          backgroundColor: "secondary.main",
+          padding: 5,
+          color: "secondary.main",
+          borderRadius: 10,
+        }}
+        spacing={2}
+      >
+        <Grid item xs={12}>
+          <Typography variant="h3" color="primary.main">
+            {articleData.name}
+          </Typography>
+        </Grid>
+
+        {/* first section */}
+        <Grid item xs={5.8}>
+          <Grid item></Grid>
           {/* documents */}
           <Grid item>
             <img src={myImg} alt="" />
           </Grid>
-          <Grid container sx={{ mt: 2 }} spacing={2}>
+          <Grid container spacing={2}>
             {/* TODO: map throught your documents here */}
-            {Object.keys(myImgss).map((key, index) => (
+            {/* {Object.keys(myImgss).map((key, index) => (
               <Grid
                 item
                 xs={4}
                 key={index}
                 sx={{ height: 100, backgroundColor: "green" }}
               ></Grid>
-            ))}
+            ))} */}
           </Grid>
-        </Grid>
-        {/* seller userdata subsection */}
-        <Grid container spacing={2} sx={{ mt: 2 }}>
-          {/* title */}
-          <Grid item xs={12}>
-            <Typography variant="h3">
-              <PersonIcon fontSize="large" sx={{ mr: 2 }} />
-              le vendeur
-            </Typography>
-          </Grid>
-          {/* seller data details */}
-          <Grid item xs={5}>
-          {Object.keys(user).map((key, index) => (
-            <Grid container key={index}>
-              <Grid item xs={6}>
-                <Typography variant="h6">{key} </Typography>
-              </Grid>
-              <Grid item xs={6}>
-                <Typography variant="h6">{user[key]} </Typography>
+          {/* seller userdata subsection */}
+          <Grid container spacing={2} sx={{ mt: 2 }}>
+            {/* title */}
+            <Grid
+              item
+              xs={12}
+              sx={{
+                backgroundColor: "primary.main",
+                borderRadius: 10,
+                padding: 4,
+                mt: 5,
+              }}
+            >
+              <Typography variant="h3">
+                <PersonIcon fontSize="large" sx={{ mr: 2 }} />
+                le vendeur
+              </Typography>
+              {/* seller data details */}
+              <Grid item sx={{ mt: 2 }}>
+                {Object.keys(user).map((key, index) => (
+                  <Grid container key={index} sx={{ mb: 2 }}>
+                    <Grid item xs={6}>
+                      <Typography variant="h6">{key}: </Typography>
+                    </Grid>
+                    <Grid item xs={6}>
+                      <Typography variant="h6">{user[key]} </Typography>
+                    </Grid>
+                  </Grid>
+                ))}
               </Grid>
             </Grid>
-          ))}
           </Grid>
-          
+        </Grid>
+        <Grid item xs={0.4}></Grid>
+        {/* second section */}
+        <Grid item xs={5.8} sx={{ color: "secondary.main" }}>
+          {/* selling section */}
+          <Box
+            sx={{
+              backgroundColor: "primary.main",
+              borderRadius: 10,
+              padding: 4,
+            }}
+          >
+            <Grid container spacing={2}>
+              <Grid item xs={4}>
+                <Typography variant="h6">prix immediat:</Typography>{" "}
+              </Grid>
+              <Grid item xs={4}>
+                <Typography variant="h6" color="info.main">
+                  {enchereData.immediatePrice}TND
+                </Typography>
+              </Grid>
+              <Grid item xs={4}>
+                <Button
+                  sx={{
+                    color: "secondary.main",
+                    border:"1px solid black",
+                    "&:hover": {
+                      backgroundColor: "info.main",
+                      color: "primary.main",
+                    },
+                  }}
+                >
+                  {" "}
+                  acheter maintenant
+                </Button>
+              </Grid>
+            </Grid>
+            <hr style={{ width: "60%" }} />
+
+              <Grid container spacing={2}>
+                <Grid item xs={4}>
+                  <Typography variant="h6">encherir:</Typography>{" "}                              
+                </Grid>
+                <Grid item xs={4}>
+                  <Typography variant="h6" color="info.main">prix actuel: {enchereData.prixActuel}TND</Typography>                
+                </Grid>
+                <Grid item xs={4}> 
+
+                  <Typography variant="h8">
+                    prix apres l'augmentation:{" "}
+                    {enchereData.prixActuel + augmentation}
+                  </Typography>
+                </Grid>
+                <Grid xs={4}></Grid>
+                <Grid item xs={4}>
+                <TextField
+                    required
+                    type="number"
+                    id="augmentation"
+                    label="augmentation"
+                    value={augmentation}
+                    onChange={handleAugmentation}
+                  />
+                </Grid>
+
+                <Grid item xs={4}>
+                <Button
+                  sx={{
+                    color: "secondary.main",
+                    border:"1px solid black",
+                    "&:hover": {
+                      backgroundColor: "info.main",
+                      color: "primary.main",
+                    },
+                  }}
+                >
+                  {" "}
+                  acheter maintenant
+                </Button>
+              </Grid>
+            </Grid>
+          </Box>
+          {/* sale details section*/}
+          <Box></Box>
         </Grid>
       </Grid>
     </Grid>
